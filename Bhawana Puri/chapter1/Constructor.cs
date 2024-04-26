0f38c20994143af9c520eb66fdfb96af34dbb859
Bhawana-P <bhawanapuri763@gmail.com>
2024-04-26 14:57:22 +0545

    feat:.net

---
+++ Bhawana Puri/chapter1/Constructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter1
+{
+    internal class Constructor
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bhawana");
+        }
+    }
+}
---
+++ Bhawana Puri/chapter1/polymorphism.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter1
+{
+    internal class polymorphism
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Guys");
+        }
+    }
+}
